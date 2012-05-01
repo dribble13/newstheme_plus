86dc8b334130db1141b6a322da588f6794082376
Kouichi Kishikami <kishikami@libero-tecnologia.net>
2012-05-01 17:25:21 +0900

    カテゴリーとアーカイブの表示位置をwordpress側で制御するため削除

--- newstheme/sidebar.php
+++ newstheme/sidebar.php
@@ -1,15 +1,5 @@
 	<div id="sidebar">
 		<div class="orange-box">		
-					
-			<h3>カテゴリー</h3>
-			<ul>
-				<?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?> 
-			</ul>
-			<h3>アーカイブ</h3>
-			<ul>
-			   <?php wp_get_archives('type=monthly'); ?>
-			</ul>
-			
 			<?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar(1) ) : else : ?>
 			<?php endif; ?>
 
