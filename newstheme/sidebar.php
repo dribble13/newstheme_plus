930a91c70d1fe3eeba7cb39aa0590594f72b34f7
Kouichi Kishikami <kishikami@libero-tecnologia.net>
2012-05-08 00:30:25 +0900

    サイドバーにブログ著者の情報を載せるように修正

--- newstheme/sidebar.php
+++ newstheme/sidebar.php
@@ -1,5 +1,9 @@
 	<div id="sidebar">
 		<div class="orange-box">		
+		  <h3><?php the_author_meta('display_name') ?>について</h3>
+		  <ul>
+		    <?php the_author_meta('description') ?>
+		  </ul>
 			<?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar(1) ) : else : ?>
 			<?php endif; ?>
 
